495f0c3992292214f1b98f09a9be9bf21c78688c
Giselle Souza <giselle_souza97@yahoo.com.br>
2022-07-18 16:12:57 -0300

    Security config

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://github.com/giselle-souza">Giselle Souza</a>
+                    <a href="https://github.com/giselle-souza"> Giselle Souza</a>
                 </p>
             </div>
         </header>
